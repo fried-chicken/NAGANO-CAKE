641d06be601ae1cfa97ee15e931db1cd529ec75f
EC2 Default User <ec2-user@ip-172-31-32-194.ap-northeast-1.compute.internal>
2024-02-17 08:09:21 +0000

    mypage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!, if: :admin_url
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   def admin_url
     request.fullpath.include?("/admin")
@@ -11,6 +12,12 @@ class ApplicationController < ActionController::Base
     else
       new_customer_session_path
     end
+  end
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number,])
   end
 
 end
