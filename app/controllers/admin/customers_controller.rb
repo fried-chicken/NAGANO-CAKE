2b33cb78ab01e0b48541c95a0ce8e305343bdaf6
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-02-18 08:33:38 +0000

    [Update]顧客編集　記述完了　form_with位置修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,13 +14,13 @@ class Admin::CustomersController < ApplicationController
   def update
     customer = Customer.find(params[:id])
     customer.update(customer_params)
-    redirect_to admin_cutomers_path(customer)
+    redirect_to admin_customer_path(customer)
   end
 
   private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :postal_code, :phone_nuber, :is_active)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :postal_code, :phone_number, :is_active)
   end
 
 end
