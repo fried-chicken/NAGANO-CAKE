f6190c59535f2e80f748dda904fd095e6b271189
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-02-21 11:49:31 +0000

    [Add]注文履歴一覧

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module Demo2
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
+    config.time_zone = 'Asia/Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
