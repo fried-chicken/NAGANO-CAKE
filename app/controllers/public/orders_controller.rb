82cf615ede53c6c7b18548a8c30f60e08281057c
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-02-23 12:52:56 +0000

    [Update]レイアウト調整　テーブル背景色変更　バリデーション途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,6 +10,7 @@ class Public::OrdersController < ApplicationController
   def confirm
      @shipping_fee = 800
      @order = Order.new(order_params)
+
     if params[:order][:select_address] == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
@@ -22,28 +23,30 @@ class Public::OrdersController < ApplicationController
     elsif params[:order][:select_address] == "2"
       @order.customer_id = current_customer.id
     end
+
       @cart_items = current_customer.cart_items
       @order_new = Order.new
+
       render :confirm
   end
 
   def create
     order = Order.new(order_params)
     order.save
-    @cart_items = current_customer.cart_items.all
+      @cart_items = current_customer.cart_items.all
 
-    @cart_items.each do |cart_item|
-      @order_details = OrderDetail.new
-      @order_details.order_id = order.id
-      @order_details.item_id = cart_item.item.id
-      @order_details.price = cart_item.item.price
-      @order_details.amount = cart_item.amount
-      @order_details.making_status = 0
-      @order_details.save!
-    end
+        @cart_items.each do |cart_item|
+          @order_details = OrderDetail.new
+          @order_details.order_id = order.id
+          @order_details.item_id = cart_item.item.id
+          @order_details.price = cart_item.item.price
+          @order_details.amount = cart_item.amount
+          @order_details.making_status = 0
+          @order_details.save!
+        end
 
-    CartItem.destroy_all
-    redirect_to orders_thanks_path
+        CartItem.destroy_all
+        redirect_to orders_thanks_path
   end
 
   def thanks
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
-  
-  enum payment_method: { 
+
+  enum payment_method: {
     credit_card: 0,
     transfer: 1
   }
@@ -14,4 +14,10 @@ class Order < ApplicationRecord
     発送準備中: 3,
     発送済み: 4
   }
+
+  validates :payment_method, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
