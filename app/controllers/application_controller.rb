96016e89fa38cf006fbe0bd7070f2d2737ee0ef9
EC2 Default User <ec2-user@ip-172-31-32-194.ap-northeast-1.compute.internal>
2024-02-23 04:42:39 +0000

    a-top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when Admin
-      admin_items_path
+      admin_orders_path
     when Customer
       items_path
     else
