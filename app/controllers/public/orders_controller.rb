76364d642723025aa87254d52e76c0a25a43adb1
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-02-24 08:52:06 +0000

    [Update]コントローラ　修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,7 +4,7 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     @customer = current_customer
-    @addresses = Address.all
+    @addresses = current_customer.addresses
   end
 
   def confirm
