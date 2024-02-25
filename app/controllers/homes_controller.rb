45ec0fd4864ef6e0a42433eb4ef2aaaf934128e4
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-02-25 16:05:02 +0900

    update-header

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,5 @@
 class HomesController < ApplicationController
   def top
-    @current_customer = current_customer
     @new_items = Item.where(is_active: true).order(created_at: :desc).limit(4)
     # where(is_active: true)で販売中の商品のみ取得、order(created_at: :desc).limit(4)で最新の４件まで取得
   end
