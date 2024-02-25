fe6b0c234f3494d825e8fef86bbdb3532fe0aba2
EC2 Default User <ec2-user@ip-172-31-32-194.ap-northeast-1.compute.internal>
2024-02-25 09:57:40 +0000

    afterlogin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!, if: :admin_url
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def admin_url
     request.fullpath.include?("/admin")
   end
-  
+
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :admin
       new_admin_session_path
@@ -13,18 +13,18 @@ class ApplicationController < ActionController::Base
       new_customer_session_path
     end
   end
-  
+
   def after_sign_in_path_for(resource)
     case resource
     when Admin
       admin_orders_path
     when Customer
-      customers_mypage_path
+      root_path
     else
       items_path
     end
   end
-  
+
   protected
 
   def configure_permitted_parameters
